f5f143c5d2dec7433585b47d58dd0069599c4ac4
MooVC <50200071+MooVC@users.noreply.github.com>
2022-10-25 14:50:46 +0100

    Updated to target MooVC 7.x (**Breaking Change**)

--- src/MooVC.Infrastructure.Compression.LZ4.Tests/CompressorTests/WhenBytesAreCompressed.cs
+++ src/MooVC.Infrastructure.Compression.LZ4.Tests/CompressorTests/WhenBytesAreCompressed.cs
@@ -1,28 +1,27 @@
-﻿namespace MooVC.Infrastructure.Compression.LZ4.CompressorTests
+﻿namespace MooVC.Infrastructure.Compression.LZ4.CompressorTests;
+
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using Xunit;
+
+public sealed class WhenBytesAreCompressed
 {
-    using System.Collections.Generic;
-    using System.Security.Cryptography;
-    using System.Threading.Tasks;
-    using Xunit;
-
-    public sealed class WhenBytesAreCompressed
+    [Fact]
+    public async Task GivenBytesThenTheResultMatchesAsync()
     {
-        [Fact]
-        public async Task GivenBytesThenTheResultMatchesAsync()
-        {
-            byte[] expected = new byte[32768];
-            var random = RandomNumberGenerator.Create();
+        byte[] expected = new byte[32768];
+        var random = RandomNumberGenerator.Create();
 
-            random.GetNonZeroBytes(expected);
+        random.GetNonZeroBytes(expected);
 
-            var compressor = new Compressor();
-            IEnumerable<byte> compressed = await compressor.CompressAsync(expected);
+        var compressor = new Compressor();
+        IEnumerable<byte> compressed = await compressor.CompressAsync(expected);
 
-            Assert.NotEqual(expected, compressed);
+        Assert.NotEqual(expected, compressed);
 
-            IEnumerable<byte> decompressed = await compressor.DecompressAsync(compressed);
+        IEnumerable<byte> decompressed = await compressor.DecompressAsync(compressed);
 
-            Assert.Equal(expected, decompressed);
-        }
+        Assert.Equal(expected, decompressed);
     }
 }
